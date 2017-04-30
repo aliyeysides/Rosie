3f4f5443705ceff09de087a282ac203d8e98fcb1
Ali Yeysides <ali.yeysides@gmail.com>
2017-04-30 12:03:55 -0400

    update project

---
+++ Rosie/APIKeys.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rosie
+{
+    public static partial class APIKeys
+    {
+        public static string DiscordClientToken;
+    }
+}
---
+++ Rosie/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Rosie
+{
+	public class Program
+    {
+        public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            var client = new DiscordSocketClient();
+
+            client.Log += Log;
+
+            string token = APIKeys.DiscordClientToken;
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+
+}
